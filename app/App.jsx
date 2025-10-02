dffed9d463c28c42d954fc63a57fc79218d3c0c4
Thiago <thiagoguimmaraess@gmail.com>
2025-10-02 10:27:27 -0300

    Corrigido caminho dos arquivos

--- app/App.jsx
+++ app/App.jsx
@@ -1,8 +1,8 @@
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Community from "./components/Community";
-import Footer from "./components/Footer";
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import Features from "@/components/Features";
+import Community from "@/components/Community";
+import Footer from "@/components/Footer";
 
 function App() {
   return (
