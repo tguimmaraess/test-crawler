3081a698be2f56d0bf1682d3fc9764c506e1ebb8
Thiago <thiagoguimmaraess@gmail.com>
2025-10-02 10:32:36 -0300

    Corrigido caminho dos arquivos

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero";
-import Features from "@/components/Features";
-import Community from "@/components/Community";
-import Footer from "@/components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Community from "./components/Community";
+import Footer from "./components/Footer";
+
 
 function Home() {
   return (
