9bbacd1235db1ac711d45be9515b8fe0ed595c1e
Thiago <thiagoguimmaraess@gmail.com>
2025-10-02 10:18:25 -0300

    Adicionado tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",       // para Next 13+ usando /app
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
