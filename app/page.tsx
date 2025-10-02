1c4fcc330b0647c824d64baadb5eae64f85e231c
Thiago <thiagoguimmaraess@gmail.com>
2025-10-02 10:17:22 -0300

    Removido mportação css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Community from "./components/Community";
 import Footer from "./components/Footer";
-import './globals.css';
 
 function Home() {
   return (
