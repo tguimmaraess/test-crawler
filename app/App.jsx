93f7477a20e0bc8452cc2392d19acb422a90c84d
Thiago <thiagoguimmaraess@gmail.com>
2025-10-02 08:26:42 -0300

    Initial commit from Create Next App

---
+++ app/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Community from "./components/Community";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen w-full bg-white text-gray-900 scroll-smooth">
+      <Header />
+      <main className="w-full">
+        <Hero />
+        <Features />
+        <Community />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
