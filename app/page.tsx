c9841f534385321d0223b164b352be2bf7380639
Thiago <thiagoguimmaraess@gmail.com>
2025-10-02 10:15:56 -0300

    Importação css

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Community from "./components/Community";
 import Footer from "./components/Footer";
+import './globals.css';
 
 function Home() {
   return (
